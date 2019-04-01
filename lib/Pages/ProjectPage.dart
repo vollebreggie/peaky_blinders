9f9f9107298ecd82c7a37dfe8d89cf9b2c8f2d25
vollebreggie <mikevol@live.nl>
2019-04-01 17:12:40 +0200

    layout changes

--- lib/Pages/ProjectPage.dart
+++ lib/Pages/ProjectPage.dart
@@ -17,96 +17,105 @@ class ProjectPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
+        title: new TextFormField(
+          textAlign: TextAlign.left,
+          decoration: new InputDecoration(
+            border: InputBorder.none,
+            hintText: 'Project title',
+            fillColor: Colors.white,
+          ),
+        ),
         backgroundColor: Colors.transparent,
       ),
       body: Center(
-        child: new Column(
-          children: [
-            new TextFormField(
-              cursorColor: Colors.white,
-              decoration: new InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                    borderSide: BorderSide(color: Colors.white)),
-                labelText: "Project Title",
-                labelStyle: TextStyle(color: Colors.white),
-                fillColor: Colors.white,
-                border: new OutlineInputBorder(
-                  borderRadius: new BorderRadius.circular(25.0),
-                  borderSide: new BorderSide(color: Colors.white),
-                ),
-                //fillColor: Colors.green
-              ),
-              validator: (val) {
-                if (val.length == 0) {
-                  return "Title cannot be empty";
-                } else {
-                  return null;
-                }
-              },
-              //keyboardType: TextInputType.emailAddress,
-              style: new TextStyle(fontFamily: "Poppins", color: Colors.white),
-            ),
-            new TextFormField(
-              cursorColor: Colors.white,
-              keyboardType: TextInputType.multiline,
-              maxLines: 7,
-              decoration: new InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                    borderSide: BorderSide(color: Colors.white)),
-                labelText: "Description",
-                labelStyle: TextStyle(color: Colors.white),
-                fillColor: Colors.white,
-                border: new OutlineInputBorder(
-                  borderRadius: new BorderRadius.circular(25.0),
-                  borderSide: new BorderSide(color: Colors.white),
-                ),
-              ),
-              style: new TextStyle(fontFamily: "Poppins", color: Colors.white),
-            ),
-            new CarouselSlider(
-              height: 200.0,
-              items: [
-                "ironman.png",
-                "splashscreen.jpg",
-                "profile.jpg",
-                "background.png",
-                "dashboard.jpg"
-              ].map((image) {
-                return Builder(
-                  builder: (BuildContext context) {
-                    return Container(
-                        decoration: new BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(25.0)),
-                      image: new DecorationImage(
-                        image: new AssetImage("assets/$image"),
-                        fit: BoxFit.fill,
-                      ),
-                    ));
-                  },
-                );
-              }).toList(),
-            ),
-            StreamBuilder<List<ProjectTask>>(
-                stream: taskBloc.outProjectTask,
-                initialData: [],
-                builder: (BuildContext context,
-                    AsyncSnapshot<List<ProjectTask>> snapshot) {
-                  taskBloc.fetchProjectTask.add(null);
-                  return Container(
-                    // decoration: BoxDecoration(color: Color.fromRGBO(58, 66, 86, 1.0)),
-                    child: ListView.builder(
-                      scrollDirection: Axis.vertical,
-                      shrinkWrap: true,
-                      itemCount: snapshot.data.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return makeCard(snapshot.data[index]);
-                      },
+        child: SingleChildScrollView(
+          child: new Column(
+            children: [
+              new Container(
+                  height: MediaQuery.of(context).size.height,
+                  decoration: new BoxDecoration(
+                    image: new DecorationImage(
+                      image: new AssetImage("assets/splashscreen.png"),
+                      fit: BoxFit.fill,
                     ),
-                  );
-                })
-          ],
+                  ),
+                  child: new Column(children: [
+                    new CarouselSlider(
+                      height: 200.0,
+                      items: [
+                        "splashscreen.jpg",
+                        "profile.jpg",
+                        "map.png",
+                        "dashboard.jpg"
+                      ].map((image) {
+                        return Builder(
+                          builder: (BuildContext context) {
+                            return Container(
+                                decoration: new BoxDecoration(
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(25.0)),
+                              image: new DecorationImage(
+                                image: new AssetImage("assets/$image"),
+                                fit: BoxFit.fill,
+                              ),
+                            ));
+                          },
+                        );
+                      }).toList(),
+                    ),
+                    new Card(
+                      elevation: 8.0,
+                      margin: new EdgeInsets.symmetric(
+                          horizontal: 5.0, vertical: 6.0),
+                      color: Color.fromRGBO(209, 224, 224, 0.4),
+                      child: Container(
+                        height: 200,
+                        padding: EdgeInsets.all(10.0),
+                        child: new ConstrainedBox(
+                          constraints: BoxConstraints(
+                            maxHeight: 200.0,
+                          ),
+                          child: new Scrollbar(
+                            child: new SingleChildScrollView(
+                              scrollDirection: Axis.vertical,
+                              reverse: true,
+                              child: SizedBox(
+                                height: 190.0,
+                                child: new TextField(
+                                  textAlign: TextAlign.center,
+                                  maxLines: 100,
+                                  decoration: new InputDecoration(
+                                    border: InputBorder.none,
+                                    hintText: 'Description of the project',
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ])),
+              StreamBuilder<List<ProjectTask>>(
+                  stream: taskBloc.outProjectTask,
+                  initialData: [],
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<ProjectTask>> snapshot) {
+                    taskBloc.fetchProjectTask.add(null);
+                    return Container(
+                      color: Color.fromRGBO(58, 66, 86, 1.0),
+                      child: ListView.builder(
+                        scrollDirection: Axis.vertical,
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return makeCard(snapshot.data[index]);
+                        },
+                      ),
+                    );
+                  })
+            ],
+          ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
